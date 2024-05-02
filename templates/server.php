be468033a1279751281bdc6daaf1d2a8ed2ccb66
collinblank <cablank25@gmail.com>
2024-05-02 14:40:17 -0400

    update with path to server-form

--- templates/server.php
+++ templates/server.php
@@ -8,6 +8,6 @@
         <div id="server-heading">
             <h1>Manage Servers</h1>
         </div>
-        <div id="add-server-btn"><form action="Grant's file name goes here" method="get"><input type="submit" value="Add Server"></div>
+        <div id="add-server-btn"><form action="parts/forms/server-form.php" method="get"><input type="submit" value="Add Server"></div>
     </div>
 </section>
