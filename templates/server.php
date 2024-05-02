acf0c70c96697476fa2c97e2fa3817e50d532674
collinblank <cablank25@gmail.com>
2024-05-02 09:39:27 -0400

    set up button to try action of getting grant's file

--- templates/server.php
+++ templates/server.php
@@ -1,13 +1,13 @@
 <?php
 ?>
 <script>
-    
+
 </script>
 <section>
     <div class="server-header">
         <div id="server-heading">
             <h1>Manage Servers</h1>
         </div>
-        <div id="add-server-btn"><button>Add Server</button></div>
+        <div id="add-server-btn"><form action="Grant's file name goes here" method="get"><input type="submit" value="Add Server"></div>
     </div>
 </section>
