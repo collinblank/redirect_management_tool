0a2cbf33ec38fe962dc01c5f1263aaa0c7b74420
collinblank <cablank25@gmail.com>
2024-05-02 09:34:03 -0400

    add new template directory and server file

---
+++ templates/server.php
@@ -0,0 +1,13 @@
+<?php
+?>
+<script>
+    
+</script>
+<section>
+    <div class="server-header">
+        <div id="server-heading">
+            <h1>Manage Servers</h1>
+        </div>
+        <div id="add-server-btn"><button>Add Server</button></div>
+    </div>
+</section>
