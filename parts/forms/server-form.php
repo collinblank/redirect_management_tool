3c9a34aa9a545c062d6e4de86ade4ce7d40787f6
collinblank <cablank25@gmail.com>
2024-05-06 13:38:10 -0400

    finding issue with db connection

--- parts/forms/server-form.php
+++ parts/forms/server-form.php
@@ -1,17 +1,17 @@
 <?php
-// $servername = DB_HOST;
-// $username = DB_USER;
-// $password = DB_PASSWORD;
-// $dbname = DB_NAME;
-// $sql = "mysql:host=$servername;dbname=$dbname;";
-// $dsn_Options = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION];
-// // Create a new connection to the MySQL database using PDO, $my_Db_Connection is an object
-// try {
-//     $my_Db_Connection = new PDO($sql, $username, $password, $dsn_Options);
-//     echo "Connected successfully";
-// } catch (PDOException $error) {
-//     echo 'Connection error: ' . $error->getMessage();
-// }
+$servername = DB_HOST;
+$username = DB_USER;
+$password = DB_PASSWORD;
+$dbname = DB_NAME;
+$sql = "mysql:host=$servername;dbname=$dbname;";
+$dsn_Options = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION];
+// Create a new connection to the MySQL database using PDO, $my_Db_Connection is an object
+try {
+    $my_Db_Connection = new PDO($sql, $username, $password, $dsn_Options);
+    echo "Connected successfully";
+} catch (PDOException $error) {
+    echo 'Connection error: ' . $error->getMessage();
+}
 // // Set the variables for the person we want to add to the database
 // $name = filter_input(INPUT_POST, 'server_name', FILTER_SANITIZE_STRING);
 // $domain = filter_input(INPUT_POST, 'server_domain', FILTER_SANITIZE_STRING);
