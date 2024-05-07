acf4b51b58d6386b54407293056be010cf3cc13e
Grant Collins <grantcollins03@gmail.com>
2024-05-07 10:51:31 -0400

    enqueue custom scripts to footer

--- src/js/custom-scripts.js
+++ src/js/custom-scripts.js
@@ -1,18 +1,18 @@
-// const modal = document.querySelector(".modal");
-// const toggleModalBtns = document.querySelectorAll(".toggle-modal-btn");
-// console.log("script called!");
+const modal = document.querySelector(".modal");
+const toggleModalBtns = document.querySelectorAll(".toggle-modal-btn");
+console.log("script called!");
 
-// toggleModalBtns.forEach((btn) => {
-//   btn.addEventListener("click", function () {
-//     toggleModal(modal);
-//     console.log("button click works!");
-//   });
-// });
+toggleModalBtns.forEach((btn) => {
+  btn.addEventListener("click", function () {
+    toggleModal(modal);
+    console.log("button click works!");
+  });
+});
 
-// function toggleModal(modal) {
-//   if (modal.classList.contains("active")) {
-//     modal.classList.remove("active");
-//   } else {
-//     modal.classList.add("active");
-//   }
-// }
+function toggleModal(modal) {
+  if (modal.classList.contains("active")) {
+    modal.classList.remove("active");
+  } else {
+    modal.classList.add("active");
+  }
+}
