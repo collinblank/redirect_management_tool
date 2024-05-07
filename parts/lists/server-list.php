b677775b567145069a6950c3257bf8b5428a9da8
Grant Collins <grantcollins03@gmail.com>
2024-05-07 15:18:56 -0400

    added server list view

---
+++ parts/lists/server-list.php
@@ -0,0 +1,21 @@
+<?php
+global $wpdb;
+$results = $wpdb->get_results("SELECT * FROM Servers");
+?>
+
+<ul class="server-list-view list-view">
+    <?php
+    foreach ($results as $server) { ?>
+        <li class="list-view__item">
+            <div class="list-view__item__info">
+                <h4><?php echo $server->name; ?></h4>
+                <p><?php echo $server->domain; ?></p>
+            </div>
+            <!-- <div class="list-view__item__btns-container">
+                <button>Edit</button>
+                <button>Disable</button>
+                <button>View Sites</button>
+            </div> -->
+        </li>
+    <?php } ?>
+</ul>
--- templates/server.php
+++ templates/server.php
@@ -1,11 +1,12 @@
 <?php /* Template Name: Server */ ?>
 <?php get_header(); ?>
-<section class="server-container">
-    <div class="server-container__header">
-        <div id="server-heading">
-            <h1>Manage Servers</h1>
-        </div>
+<section class="page-section">
+    <div class="page-section__header">
+        <h1>Manage Servers</h1>
         <button id="add-server-btn" class="toggle-modal-btn">Add Server</button>
+    </div>
+    <div class="page-section__list-view-container">
+        <?php get_template_part('parts/lists/server-list'); ?>
     </div>
     <div class="modal">
         <?php get_template_part('parts/forms/server-form'); ?>
