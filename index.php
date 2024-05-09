d394f280d632efc9618205796da8dd01b41841dc
Grant Collins <grantcollins03@gmail.com>
2024-05-09 14:10:22 -0400

    comment out extra parts

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 get_header();
-if (have_posts()) : while (have_posts()) : the_post();
-        get_template_part('entry');
-        comments_template();
-    endwhile;
-endif;
 get_template_part('templates/home');
-get_template_part('nav', 'below');
-get_footer();
+// if (have_posts()) : while (have_posts()) : the_post();
+//         get_template_part('entry');
+//         comments_template();
+//     endwhile;
+// endif;
+// get_template_part('nav', 'below');
+// get_footer();
