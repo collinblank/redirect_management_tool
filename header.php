7b3e9a193e5ddda45284fc4c8a02f1cb71b45906
Grant Collins <grantcollins03@gmail.com>
2024-05-09 15:17:37 -0400

    reset styles

--- header.php
+++ header.php
@@ -4,6 +4,11 @@
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width">
+    <!-- Start Google Font Inter -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
+    <!-- End Google Font Inter -->
     <?php wp_head(); ?>
 </head>
 
