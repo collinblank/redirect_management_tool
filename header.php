e705e80b013eab4fab1113d7e7040be7cbc78974
Grant Collins <grantcollins03@gmail.com>
2024-05-13 11:32:59 -0400

    switch to font awesome icons

--- header.php
+++ header.php
@@ -9,6 +9,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
     <!-- End Google Font Inter -->
+    <script src="https://kit.fontawesome.com/3feedd961e.js" crossorigin="anonymous"></script>
     <?php wp_head(); ?>
 </head>
 
