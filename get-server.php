1cdfd1d6416d240d0e42d2035a31a239b5e1b186
Grant Collins <grantcollins03@gmail.com>
2024-05-16 16:41:20 -0400

    comment out include statement

--- get-server.php
+++ get-server.php
@@ -1,12 +1,11 @@
 <?php
 global $wpdb;
 $id = isset($_POST['item_id']) ? $_POST['item_id'] : '';
-$server = $wpdb->get_row("SELECT * FROM Servers WHERE Id = " . $id, ARRAY_A);
+$server = $wpdb->get_row("SELECT * FROM $wpdb->Servers WHERE Id = " . $id, ARRAY_A);
 
 if ($server) {
     echo json_encode($server);
 } else {
     echo "<script>console.log('Unable to save Server');</script>";
 }
-
-include 'parts/modals/disable-server-modal.php';
+// include 'parts/modals/disable-server-modal.php';
