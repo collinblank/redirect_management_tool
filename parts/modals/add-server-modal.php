0e1d2786f2d5e2797898375a738722e7880c1e10
Grant Collins <grantcollins03@gmail.com>
2024-06-03 08:49:14 -0400

    create general functions for modals

--- parts/modals/add-server-modal.php
+++
@@ -1,3 +0,0 @@
-<div class="modal add-modal">
-    <?php get_template_part('parts/forms/server-form'); ?>
-</div>
--- src/js/custom-scripts.js
+++ src/js/custom-scripts.js
@@ -9,11 +9,16 @@ successMsgXBtn.addEventListener("click", () => {
 });
 
 const addServerBtn = document.getElementById("add-server-btn");
-addServerBtn.addEventListener("click", showAddServerModal);
+addServerBtn.addEventListener("click", () => {
+  showAddModal("server");
+});
 
+// Just for servers right now
 const disableBtns = document.querySelectorAll(".disable-btn");
 disableBtns.forEach((btn) => {
-  btn.addEventListener("click", handleDisableBtnClick);
+  btn.addEventListener("click", (e) => {
+    handleDisableBtnClick(e, "Servers", showDisableServerModal);
+  });
 });
 
 const cancelBtns = document.querySelectorAll(".cancel-btn");
@@ -23,12 +28,10 @@ cancelBtns.forEach((btn) => {
 
 // --------
 
-// Just for servers right now
-function handleDisableBtnClick(e) {
+function handleDisableBtnClick(e, tableName, showDisableModalFunc) {
   const itemId = e.target.closest(".list-view__item").dataset.itemId;
-  const tableName = "Servers";
-  const params = `?table_name=${tableName}&item_id=${itemId}`;
   const filePath = "/wp-content/themes/redirect-management-tool/get-item.php";
+  const params = `?table_name=${tableName}&item_id=${itemId}`;
   const url = filePath + params;
   const xhr = new XMLHttpRequest();
 
@@ -39,7 +42,7 @@ function handleDisableBtnClick(e) {
       if (xhr.status === 200) {
         const item = JSON.parse(xhr.responseText);
         console.log(item);
-        showDisableServerModal(item);
+        showDisableModalFunc(item);
       } else {
         console.error("Request failed with status: " + xhr.status);
         console.error("Response text: " + xhr.responseText);
@@ -52,14 +55,30 @@ function handleDisableBtnClick(e) {
 
 // just for servers right now
 function showDisableServerModal(item) {
-  const params = `?server_name=${item.Name}&server_domain=${item.Domain}`;
   const filePath = "parts/modals/disable-server-modal.php";
+  const params = `?server_name=${item.Name}&server_domain=${item.Domain}`;
   showModal(filePath, params);
 }
 
-function showAddServerModal() {
-  const filePath = "parts/modals/add-server-modal.php";
-  showModal(filePath);
+// something like this for a general function
+// function showDisableModal(item) {
+//   const filePath;
+//   const params = `?item_name=${item.Name}&item_info=${item.Domain ? item.Domain : item.redirectFlagRule}`;
+//   // the above params need an item_type somehow. For determing whether it's a website, server, etc.
+//   showModal(filePath, params);
+// }
+
+// each add function needs to be specific because each form is different. Or I can pass the forms in via
+// function showAddServerModal() {
+//   const filePath = "parts/modals/add-server-modal.php";
+//   showModal(filePath);
+// }
+
+// attempt at generic function for add modals
+function showAddModal(itemType) {
+  const filePath = "parts/modals/add-modal.php";
+  const params = `?item_type=${itemType}`;
+  showModal(filePath, params);
 }
 
 function showModal(modalFilePath, params = "") {
@@ -74,6 +93,7 @@ function showModal(modalFilePath, params = "") {
     if (xhr.readyState === XMLHttpRequest.DONE) {
       if (xhr.status === 200) {
         const modal = xhr.responseText;
+        console.log(modal);
         mainContent.insertAdjacentHTML("beforeend", modal);
       } else {
         console.error("Request failed with status: " + xhr.status);
