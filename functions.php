056bd3c2e6bc605d3b928c16f32a8ad1a554d8ba
Grant Collins <grantcollins03@gmail.com>
2024-06-12 18:40:50 -0400

    redirect with get permalink

--- functions.php
+++ functions.php
@@ -309,9 +309,9 @@ if (isset($_POST['add_server'])) {
 
 	if (!empty($errors)) {
 		$_SESSION['form_errors'] = $errors;
-		wp_redirect(home_url('servers'), 303);
+		$new_url = add_query_arg('errors', $count($errors), get_permalink());
+		wp_redirect($new_url, 303);
 		exit;
-		return false;
 	} else {
 		$table_name = 'servers';
 		$data = array(
