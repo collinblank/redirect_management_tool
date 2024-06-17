08dd8401055738534309c0546d582de49c1cb6f8
Grant Collins <grantcollins03@gmail.com>
2024-06-17 17:14:21 -0400

    mv form validation logic back to custom scripts

--- src/js/custom-scripts.js
+++ src/js/custom-scripts.js
@@ -80,94 +80,94 @@ function showModal(action, e) {
 }
 
 // FORM VALIDATION
-// function initFormValidation(tableName) {
-//   if (tableName === "servers") {
-//     initServerFormValidation();
-//   }
+function initFormValidation(tableName) {
+  if (tableName === "servers") {
+    initServerFormValidation();
+  }
 
-//   const form = document.querySelector(".modal-form");
-//   const submitBtn = form.querySelector('input[type="submit"]');
-//   const inputs = form.querySelectorAll(".form__input-item input");
+  const form = document.querySelector(".modal-form");
+  const submitBtn = form.querySelector('input[type="submit"]');
+  const inputs = form.querySelectorAll(".form__input-item input");
 
-//   function initServerFormValidation() {
-//     const serverNameInput = document.getElementById("server-name");
-//     const serverDomainInput = document.getElementById("server-domain");
-//     // EVENT LISTENERS
-//     initInputEvents(serverNameInput, checkServerName);
-//     initInputEvents(serverDomainInput, checkServerDomain);
+  function initServerFormValidation() {
+    const serverNameInput = document.getElementById("server-name");
+    const serverDomainInput = document.getElementById("server-domain");
+    // EVENT LISTENERS
+    initInputEvents(serverNameInput, checkServerName);
+    initInputEvents(serverDomainInput, checkServerDomain);
 
-//     function checkServerName() {
-//       const responseMsgs = {
-//         error: "Please enter between 4 and 50 letters and spaces only.",
-//         success: "Awesome!",
-//       };
-//       checkServerInput(serverNameInput, responseMsgs);
-//     }
+    function checkServerName() {
+      const responseMsgs = {
+        error: "Please enter between 4 and 50 letters and spaces only.",
+        success: "Awesome!",
+      };
+      checkServerInput(serverNameInput, responseMsgs);
+    }
 
-//     function checkServerDomain() {
-//       const responseMsgs = {
-//         error: "Please enter a valid URL (including http(s)://).",
-//         success: "Great!",
-//       };
-//       checkServerInput(serverDomainInput, responseMsgs);
-//     }
+    function checkServerDomain() {
+      const responseMsgs = {
+        error: "Please enter a valid URL (including http(s)://).",
+        success: "Great!",
+      };
+      checkServerInput(serverDomainInput, responseMsgs);
+    }
 
-//     function checkServerInput(input, responseMsgs) {
-//       const msg = input.nextElementSibling;
-//       // const patternMismatch = !pattern.test(input.value);
+    function checkServerInput(input, responseMsgs) {
+      const msg = input.nextElementSibling;
+      // const patternMismatch = !pattern.test(input.value);
 
-//       if (!input.validity.valid) {
-//         if (input.validity.valueMissing) {
-//           msg.textContent = "Please enter a value.";
-//         } else {
-//           msg.textContent = responseMsgs.error;
-//         }
-//         input.classList.remove("valid");
-//         input.classList.add("invalid");
-//         msg.classList.remove("success", "active");
-//         msg.classList.add("error", "active");
-//       } else {
-//         msg.textContent = responseMsgs.success;
-//         input.classList.remove("invalid");
-//         input.classList.add("valid");
-//         msg.classList.remove("error", "active");
-//         msg.classList.add("success", "active");
-//       }
-//     }
-//   }
+      if (!input.validity.valid) {
+        if (input.validity.valueMissing) {
+          msg.textContent = "Please enter a value.";
+        } else {
+          msg.textContent = responseMsgs.error;
+        }
+        input.classList.remove("valid");
+        input.classList.add("invalid");
+        msg.classList.remove("success", "active");
+        msg.classList.add("error", "active");
+      } else {
+        msg.textContent = responseMsgs.success;
+        input.classList.remove("invalid");
+        input.classList.add("valid");
+        msg.classList.remove("error", "active");
+        msg.classList.add("success", "active");
+      }
+    }
+  }
 
-//   function initInputEvents(input, checkInputFunc) {
-//     let blurredOnce = false;
+  function initInputEvents(input, checkInputFunc) {
+    let blurredOnce = false;
 
-//     input.addEventListener("focus", () => {
-//       submitBtn.disabled = !allInputsValid();
-//     });
+    input.addEventListener("focus", () => {
+      submitBtn.disabled = !allInputsValid();
+    });
 
-//     input.addEventListener("blur", () => {
-//       submitBtn.disabled = !allInputsValid();
-//       checkInputFunc();
-//       blurredOnce = true;
-//     });
+    input.addEventListener("blur", () => {
+      submitBtn.disabled = !allInputsValid();
+      checkInputFunc();
+      blurredOnce = true;
+    });
 
-//     input.addEventListener("input", () => {
-//       submitBtn.disabled = !allInputsValid();
-//       if (blurredOnce) {
-//         checkInputFunc();
-//       }
-//     });
-//   }
+    input.addEventListener("input", () => {
+      submitBtn.disabled = !allInputsValid();
+      if (blurredOnce) {
+        checkInputFunc();
+      }
+    });
+  }
 
-//   function allInputsValid() {
-//     return Array.from(inputs).every((input) => input.validity.valid);
-//   }
-// }
+  function allInputsValid() {
+    return Array.from(inputs).every((input) => input.validity.valid);
+  }
+}
 
-// function initDisableItemFormValidation() {
-//   const form = document.getElementById("disable-item__form");
-//   const checkbox = form.querySelector('input[type="checkbox"]');
-//   const disableBtn = form.querySelector('input[type="submit"]');
+function initDisableItemFormValidation() {
+  const form = document.getElementById("disable-item__form");
+  const checkbox = form.querySelector('input[type="checkbox"]');
+  const disableBtn = form.querySelector('input[type="submit"]');
 
-//   checkbox.addEventListener("change", () => {
-//     disableBtn.disabled = !checkbox.checked;
-//   });
-// }
+  checkbox.addEventListener("change", () => {
+    disableBtn.disabled = !checkbox.checked;
+  });
+}
