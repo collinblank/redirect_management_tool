279217475be7c9d4468d182f2d57bcfbe4f37a86
Grant Collins <grantcollins03@gmail.com>
2024-06-19 11:19:22 -0400

    add website files and boilerplate for list view

---
+++ templates/websites.php
@@ -0,0 +1,19 @@
+<?php /* Template Name: Websites */ ?>
+
+
+<?php get_header(); ?>
+<section class="page-section list-view-page">
+    <div class="page-content-container">
+        <?php
+        // get_template_part('parts/notice-banner.php');
+        ?>
+        <div class="list-view-page__header">
+            <h1>Manage Websites</h1>
+            <button class="default-btn" id="add-website-btn">Add Website</button>
+        </div>
+        <div class="list-view-container">
+            <?php get_template_part('parts/lists/websites-list'); ?>
+        </div>
+    </div>
+</section>
+<?php get_footer(); ?>
