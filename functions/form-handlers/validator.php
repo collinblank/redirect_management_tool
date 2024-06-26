64eca8c14a063e386590d2b372a5bb5b9de83672
Grant Collins <grantcollins03@gmail.com>
2024-06-26 18:02:52 -0400

    add rough website form server side validation

--- functions/form-handlers/validator.php
+++ functions/form-handlers/validator.php
@@ -21,4 +21,27 @@ class Validator
         $pattern = "/^[A-Za-z]+(?: [A-Za-z]+)*$/";
         return preg_match($pattern, $str);
     }
+
+    public static function item_exists_in_db($item_id, $table_name)
+    {
+        global $wpdb;
+        $results = $wpdb->get_results($wpdb->prepare("SELECT * FROM $table_name WHERE id = %d", $item_id), ARRAY_A);
+        return !empty($results);
+    }
+
+    public static function sandbox_taken($item_id)
+    {
+        global $wpdb;
+        $results = $wpdb->get_results($wpdb->prepare("SELECT * FROM websites WHERE isProd = 1 AND sandboxId = %d", $item_id), ARRAY_A);
+        return !empty($results);
+    }
+
+    public static function name_or_domain_taken($name, $domain)
+    {
+        global $wpdb;
+        $name_like = '%' . $wpdb->esc_like($name) . '%';
+        $domain_like = '%' . $wpdb->esc_like($domain) . '%';
+        $results = $wpdb->get_results($wpdb->prepare("SELECT * FROM websites WHERE name LIKE %s OR domain LIKE %s", $name_like, $domain_like), ARRAY_A);
+        return !empty($results);
+    }
 }
