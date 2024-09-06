17039f37fc4d1c727c5a5589593be648e00d4424
Grant Collins <grantcollins03@gmail.com>
2024-09-06 11:11:05 -0400

    set return output as array_a for redirect to edit

--- parts/forms/redirect-rule-form.php
+++ parts/forms/redirect-rule-form.php
@@ -2,22 +2,23 @@
 require_once($_SERVER['DOCUMENT_ROOT'] . '/wp-load.php');
 global $wpdb;
 
-
-// $website_id = intval($_GET['website_id']);
-// $website_name = $wpdb->get_var($wpdb->prepare("SELECT name FROM websites WHERE id = %d", $website_id));
 $action = $_GET['action'];
 $table_name = $_GET['table_name'];
 $item_id = intval($_GET['item_id']) ?? null;
 
+
+// $website_id = intval($_GET['website_id']);
+// $website_name = $wpdb->get_var($wpdb->prepare("SELECT name FROM websites WHERE id = %d", $website_id));
+
 // pretty sure i can rework this so it is one function to get edit item for all forms, or at least some of it. This is basically the same as the logic in the website-form.php file
 if ($action === 'edit' && isset($_GET['table_name']) && isset($item_id)) {
-    $redirect_rule_to_edit = $wpdb->get_row($wpdb->prepare("SELECT * FROM redirectRules WHERE id = %d", $item_id));
+    $redirect_rule_to_edit = $wpdb->get_row($wpdb->prepare("SELECT * FROM redirectRules WHERE id = %d", $item_id), ARRAY_A);
 }
 ?>
 
 <div class="modal-content">
     <div class="modal-content__header">
-        <h3><?php echo ucfirst($action) . " Redirect for $website_name" ?></h3>
+        <h3><?php echo ucfirst($action) . " Redirect for " ?></h3>
     </div>
     <form action="<?php echo esc_url(admin_url('admin-post.php')); ?>" method="POST" class="modal-content__section modal-form">
         <input type="hidden" name="action" value="redirect_rule_form">
@@ -31,7 +32,7 @@ if ($action === 'edit' && isset($_GET['table_name']) && isset($item_id)) {
                 <input type="text" id="redirect-rule-name" name="redirect_rule_name" placeholder="ex. Pastor Appreciation" value="<?php echo $redirect_rule_to_edit['name'] ?? "" ?>" tabindex="1" minlength="4" maxlength="50" pattern="^[A-Za-z]+(?: [A-Za-z]+)*$">
                 <p class="form__input-item__validation-msg"></p>
             </li>
-            <li class="form__inputs-container">
+            <li class="form__input-item">
                 <label for="redirect-rule-description">Description</label>
                 <textarea id="redirect-rule-description" name="redirect_rule_description" placeholder="ex. Redirect for churches and pastor appreciation week"><?php echo $redirect_rule_to_edit['description'] ?? "" ?></textarea>
                 <p class="form__input-item__validation-msg"></p>
