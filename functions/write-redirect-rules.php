5bfc422dae147625fb47a0cf329b710038bc5e06
Grant Collins <grantcollins03@gmail.com>
2024-09-12 12:42:08 -0400

    return results as ARRAY_A

--- functions/write-redirect-rules.php
+++ functions/write-redirect-rules.php
@@ -5,7 +5,7 @@ function write_redirect_rules_file($website_id)
     global $wpdb;
     $website_name = $wpdb->get_var($wpdb->prepare("SELECT name FROM websites WHERE id = %d", $website_id));
     echo "<script>console.log('Website Id: {$website_id} Website Name: {$website_name}')</script>";
-    $redirect_rules = $wpdb->get_results($wpdb->prepare("SELECT * FROM redirectRules WHERE websiteId = %d AND disabled != %d ORDER BY id", $website_id, 1));
+    $redirect_rules = $wpdb->get_results($wpdb->prepare("SELECT * FROM redirectRules WHERE websiteId = %d AND disabled != %d ORDER BY id", $website_id, 1), ARRAY_A);
 
     $hyphenated_website_name = implode("-", explode(" ", strtolower($website_name)));
     echo "<script>console.log('Hyphenated website name: {$hyphenated_website_name}')</script>";
@@ -20,7 +20,7 @@ function write_redirect_rules_file($website_id)
             $rewrite_rule_content = "#{$rule['name']}\nRewriteRule {$rule['fromURLRegex']} {$rule['toURL']}\n";
             fwrite($file, $rewrite_rule_content);
         }
-        fwrite($file, "#Website: {$website_name}\n #End Rewrite Rules - DO NOT EDIT\n");
+        fwrite($file, "#End Rewrite Rules - DO NOT EDIT\n");
 
         fclose($file);
         echo "<script>console.log('File created at {$filepath}')</script>";
