bf6511ebee8b261713f0408a14d5115aa8baf344
Grant Collins <grantcollins03@gmail.com>
2024-09-16 12:05:52 -0400

    add console logs to jquery

--- functions.php
+++ functions.php
@@ -20,7 +20,7 @@ $myUpdateChecker->setBranch('main');
 // Enque custom Javascript functions to header
 function redirect_manager_scripts_styles()
 {
-	wp_enqueue_style('styles', get_stylesheet_directory_uri() . '/src/css/custom-style.css', array(), false, 'screen');
+	// wp_enqueue_style('styles', get_stylesheet_directory_uri() . '/src/css/custom-style.css', array(), false, 'screen');
 	wp_enqueue_script('scripts', get_template_directory_uri() . '/src/js/custom-scripts.js', array(), false, array('strategy' => 'defer'));
 	wp_enqueue_script('jquery', get_template_directory_uri() . '/src/js/custom-jquery.js', array(), false, array('strategy' => 'defer'));
 }
--- src/js/custom-jquery.js
+++ src/js/custom-jquery.js
@@ -1,5 +1,8 @@
 $(document).ready(function () {
+  console.log("JQuery loaded!");
+
   $(".more-actions-btn").click(function () {
     $(this).closest(".table-actions").find(".more-actions").toggle();
+    console.log("more actions button clicked!");
   });
 });
