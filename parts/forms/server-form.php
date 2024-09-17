e8e94591db01bc35da8478b72eb0da6238f4b8e1
Grant Collins <grantcollins03@gmail.com>
2024-09-17 12:13:50 -0400

    rn form elements

--- parts/forms/server-form.php
+++ parts/forms/server-form.php
@@ -23,16 +23,16 @@ if ($action === 'edit' && isset($_GET['table_name']) && isset($item_id)) {
         <?php if ($item_id) : ?>
             <input type="hidden" name="item_id" value="<?php echo $item_id ?>">
         <?php endif; ?>
-        <ul class="form__inputs-container">
-            <li class="form__input-item">
-                <label for="server-name">Server Name<span>*</span></label>
-                <input type="text" id="server-name" name="server_name" placeholder="ex. Classical Conversations Production" value="<?php echo $server_to_edit['name'] ?? "" ?>" tabindex="1" minlength="4" maxlength="50" pattern="^[A-Za-z]+(?: [A-Za-z]+)*$" required>
-                <p class="form__input-item__validation-msg"></p>
+        <ul class="form-items-list">
+            <li class="form-item">
+                <label for="server-name" class="form-label">Server Name<span>*</span></label>
+                <input type="text" class="form-text-input" id="server-name" name="server_name" placeholder="ex. Classical Conversations Production" value="<?php echo $server_to_edit['name'] ?? "" ?>" tabindex="1" minlength="4" maxlength="50" pattern="^[A-Za-z]+(?: [A-Za-z]+)*$" required>
+                <p class="form-validation-msg"></p>
             </li>
-            <li class="form__input-item">
-                <label for="server-domain">Server Domain<span>*</span></label>
-                <input type="url" id="server-domain" name="server_domain" placeholder="ex. https://classicalconversations.com:7080/login.php" value="<?php echo $server_to_edit['domain'] ?? "" ?>" tabindex="2" maxlength="100" pattern="^https?://.*$" required>
-                <p class="form__input-item__validation-msg"></p>
+            <li class="form-item">
+                <label for="server-domain" class="form-label">Server Domain<span>*</span></label>
+                <input type="url" class="form-text-input" id="server-domain" name="server_domain" placeholder="ex. https://classicalconversations.com:7080/login.php" value="<?php echo $server_to_edit['domain'] ?? "" ?>" tabindex="2" maxlength="100" pattern="^https?://.*$" required>
+                <p class="form-validation-msg"></p>
             </li>
         </ul>
         <div class="modal-content__btns-container">
