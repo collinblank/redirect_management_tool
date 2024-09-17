60cf141ef0e8d90dc114428e55678b5c0bf7b721
Grant Collins <grantcollins03@gmail.com>
2024-09-17 17:17:07 -0400

    rm styles

--- templates/servers.php
+++ templates/servers.php
@@ -14,11 +14,11 @@ $results = $wpdb->get_results("SELECT * FROM servers", ARRAY_A);
 <section class="page-section" data-table-name="servers">
     <div class="container">
         <?php get_template_part('parts/notice-banner', 'notice-banner'); ?>
-        <div class="list-view-page__header">
+        <div class="page-header">
             <h1>Servers</h1>
             <button class="btn add-item-btn">Add Server</button>
         </div>
-        <div class="list-view-container">
+        <div class="table-container">
             <?php get_template_part('parts/lists/servers-list', null, array('results' => $results)); ?>
         </div>
     </div>
