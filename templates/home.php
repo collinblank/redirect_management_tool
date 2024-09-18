9976d1490391b9a822dc80dd20bc0e6873488302
Grant Collins <grantcollins03@gmail.com>
2024-09-18 17:27:54 -0400

    mv table styles over

--- templates/home.php
+++ templates/home.php
@@ -1,7 +1,4 @@
+<?php get_template_part('parts/sidebar', 'sidebar'); ?>
 <section id="home-page" class="page-section">
     <p>What would you like to work on?</p>
-    <?php get_template_part('parts/sidebar', 'sidebar'); ?>
-    <section class="page-section">
-
-    </section>
 </section>
