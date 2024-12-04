6bc39c0e1d10368ae8f6b8a854f4bc0d0b1a9cca
Grant Collins <grantcollins03@gmail.com>
2024-12-04 13:50:37 -0500

    add server and domain to written files

--- functions/commit-rules-to-file.php
+++ functions/commit-rules-to-file.php
@@ -3,23 +3,28 @@
 function commit_rules_to_file($website_id)
 {
     global $wpdb;
-    $website_name = $wpdb->get_var($wpdb->prepare("SELECT name FROM websites WHERE id = %d", $website_id));
+    $website = $wpdb->get_row($wpdb->prepare("SELECT * FROM websites WHERE id = %d", $website_id), ARRAY_A);
+    $hyphenated_website_name = implode("-", explode(" ", strtolower($website['name'])));
+    $current_time = date('Y-m-d H:i:s');
     $redirect_rules = $wpdb->get_results($wpdb->prepare("SELECT * FROM redirect_rules WHERE website_id = %d AND disabled != %d ORDER BY id", $website_id, 1), ARRAY_A);
-
-    $hyphenated_website_name = implode("-", explode(" ", strtolower($website_name)));
     $filepath = get_template_directory() . "/storage/new-redirects/{$hyphenated_website_name}.txt";
+
     $file = fopen($filepath, "w");
 
     if (!empty($redirect_rules)) {
         if ($file) {
-            $current_timestamp = date('Y-m-d H:i:s');
-            fwrite($file, "#Website: {$website_name}\n#Time: {$current_timestamp}\n\n#Begin Rewrite Rules - DO NOT EDIT\n");
+            $server_name = $wpdb->get_var($wpdb->prepare("SELECT name FROM servers WHERE id = %d", $website['server_id']));
+            $website_base_domain = preg_replace("/^(https?:\/\/)?(www\.)?|\/$/", '', $website['domain']);
+
+            fwrite($file, "#Server: {$server_name}\n#Domain: {$website_base_domain}\n#Time: {$current_time}\n#Begin Rewrite Rules - DO NOT EDIT\n");
+
             foreach ($redirect_rules as $rule) {
                 $name_content = $rule['name'] ? "#{$rule['name']}\n" : '';
                 $rewrite_rule_content = "RewriteRule {$rule['from_url_regex']} {$rule['to_url']}\n";
                 fwrite($file, $name_content . $rewrite_rule_content);
             }
-            fwrite($file, "\n#End Rewrite Rules - DO NOT EDIT\n");
+
+            fwrite($file, "#End Rewrite Rules - DO NOT EDIT\n");
             fclose($file);
 
             // update committed flag in database
