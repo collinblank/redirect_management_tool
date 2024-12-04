9bab65f0bef5f7a1115b5476d4577386e2098c65
Grant Collins <grantcollins03@gmail.com>
2024-12-04 14:24:38 -0500

    add success/error messages for committing rules to file

--- functions/commit-rules-to-file.php
+++ functions/commit-rules-to-file.php
@@ -4,7 +4,7 @@ function commit_rules_to_file($website_id)
 {
     global $wpdb;
     $website = $wpdb->get_row($wpdb->prepare("SELECT * FROM websites WHERE id = %d", $website_id), ARRAY_A);
-    $hyphenated_website_name = implode("-", explode(" ", strtolower($website['name'])));
+    $hyphenated_website_name = str_replace(" ", "-", strtolower($website['name']));
     $current_time = date('Y-m-d H:i:s');
     $redirect_rules = $wpdb->get_results($wpdb->prepare("SELECT * FROM redirect_rules WHERE website_id = %d AND disabled != %d ORDER BY id", $website_id, 1), ARRAY_A);
     $filepath = get_template_directory() . "/storage/new-redirects/{$hyphenated_website_name}.txt";
@@ -28,7 +28,7 @@ function commit_rules_to_file($website_id)
             fclose($file);
 
             // update committed flag in database
-            $result = $wpdb->update(
+            $updated_rows = $wpdb->update(
                 'redirect_rules', // Table name
                 array('committed' => 1), // Data to update
                 array('disabled' => 0), // Where clause (only rows where `disabled` is 0)
@@ -36,9 +36,9 @@ function commit_rules_to_file($website_id)
                 array('%d')  // Format for where clause
             );
 
-            return $result;
+            return ["committed" => $updated_rows];
         } else {
-            echo "<script>console.log('Unable to create file!')</script>";
+            return ["errors" => "Unable to write rules to file."];
         }
     }
 }
--- functions/form-handlers/commit-rules-form-submit.php
+++ functions/form-handlers/commit-rules-form-submit.php
@@ -3,14 +3,27 @@
 
 function handle_commit_rules_form_submission()
 {
+    unset($_SESSION['errors'], $_SESSION['success']);
+    session_start();
+
     if (!isset($_POST['commit_rules_form_nonce_field']) || !wp_verify_nonce($_POST['commit_rules_form_nonce_field'], 'commit_rules_form_nonce')) {
         wp_die('Error: Unable to verify form nonce.');
     } else {
         $website_id = $_POST['website_id'];
 
-        $result = commit_rules_to_file($website_id);
+        $results = commit_rules_to_file($website_id);
 
-        wp_safe_redirect(add_query_arg(array('website_id' => $website_id, 'committed' => $result), home_url('/redirect-rules')), 303);
+        $redirect_args = ["website_id" => $website_id];
+        if (isset($results["committed"])) {
+            $_SESSION['success'] = "Successfully committed {$results["committed"]} redirect rules to the site's .htaccess file.";
+            $redirect_args["committed"] = $results["committed"];
+        }
+        if (isset($results["errors"])) {
+            $_SESSION['errors'] = $results["errors"];
+            $redirect_args["errors"] = $results["errors"];
+        }
+
+        wp_safe_redirect(add_query_arg($redirect_args, home_url('/redirect-rules')), 303);
         exit;
     }
 }
